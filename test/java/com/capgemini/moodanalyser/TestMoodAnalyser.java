367bb86910e6d4cbbb9e296adb63f3f1198a2e2d
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 01:42:04 +0530

    Tested : Test Case for Given Any Mood Message Returns HAPPY is Tested

--- test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
+++ test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
@@ -9,6 +9,12 @@ public class TestMoodAnalyser {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
 		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
 		Assert.assertEquals("SAD", mood);
-
+	}
+	
+	@Test
+	public void testMoodAnalysis_GivenAnyMood_ReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in Any Mood");
+		Assert.assertEquals("HAPPY", mood);
 	}
 }
