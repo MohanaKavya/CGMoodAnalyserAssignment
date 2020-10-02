f2536af81944377e39b5d0a240ef09d8fdb85ee6
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 00:55:17 +0530

    Initiation of Mood Analyser Assignment

---
+++ main/java/com/capgemini/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package com.capgemini.moodanalyser;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
@@ -0,0 +1,5 @@
+package com.capgemini.moodanalyser;
+
+public class TestMoodAnalyser {
+
+}
