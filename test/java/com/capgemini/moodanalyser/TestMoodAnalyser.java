c7d213c1dab282adc82414f50c4946aae4bc40a9
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 02:23:18 +0530

    Refactor : Refactor Test Case to Take Sad Mood Message from Constructor

--- test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
+++ test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class TestMoodAnalyser {
 	@Test
 	public void testMoodAnalysis_GivenSadMood_ReturnSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD", mood);
 
 	}
