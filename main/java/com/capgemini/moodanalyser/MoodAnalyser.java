1bc1213b742cf437b1916ef13928a247b45977b0
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 01:01:04 +0530

    Added : Given Message - Analyse Mood and Return Sad or Happy Functionality is Added

--- main/java/com/capgemini/moodanalyser/MoodAnalyser.java
+++ main/java/com/capgemini/moodanalyser/MoodAnalyser.java
@@ -1,10 +1,29 @@
 package com.capgemini.moodanalyser;
 
+import java.util.Scanner;
+
 public class MoodAnalyser {
+	
+	public String analyseMood(String message) {
+			if(message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Enter the Message :");
+			String message = sc.nextLine();
+
+			MoodAnalyser moodAnalyser = new MoodAnalyser();
+			String mood = moodAnalyser.analyseMood(message);
+			System.out.println(mood);
+
+		}
 
 	}
 
-}
+
