da8b3b11fe98aa6aeef09ac7ddaaf647396de704
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 01:08:19 +0530

    Tested : Test Case for Given Sad Mood Message Returns SAD is Tested

--- test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
+++ test/java/com/capgemini/moodanalyser/TestMoodAnalyser.java
@@ -1,5 +1,14 @@
 package com.capgemini.moodanalyser;
 
-public class TestMoodAnalyser {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestMoodAnalyser {
+	@Test
+	public void testMoodAnalysis_GivenSadMood_ReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+		Assert.assertEquals("SAD", mood);
+
+	}
 }
